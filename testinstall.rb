5367ddae2b011973c182170c37ca541b9b64ce55
Trevor Chan <Trevor.chan@pemicro.com>
2022-06-24 11:38:17 -0400

    m

--- testinstall.rb
+++ testinstall.rb
@@ -12,11 +12,7 @@ class Testinstall < Formula
   depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+
     # system "cmake", ".", *std_cmake_args
     system "make", "install"
     bin.install "bin/local/testInstall"
